28ec29f38d4640bc25d5fb05ba46db788b5e2928
omilaeva <anastasiaomilaeva@gmail.com>
2024-09-11 15:14:44 +0300

    deployment and templates exercise

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
 
 configure({
-  views: `${Deno.cwd()}/views/`,
+    views: `${Deno.cwd()}/views/`,
 });
 
 const responseDetails = {
-  headers: { "Content-Type": "text/html;charset=UTF-8" },
+    headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
 const data = {
-  count: 0,
+    count: 0,
 };
 
 const handleRequest = async (request) => {
-  const url = new URL(request.url);
-  if (url.pathname === "/count") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
-  }
+    const url = new URL(request.url);
+    if (url.pathname === "/visits") {
+        data.count++;
+        return new Response(await renderFile("visits.eta", data), responseDetails);
+    } else if (url.pathname === "/meaning") {
+        return new Response(await renderFile("meaning.eta", data), responseDetails);
+    }
+    return new Response(await renderFile("empty.eta", data), responseDetails);
 
-  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });
